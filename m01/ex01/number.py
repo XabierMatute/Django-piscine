4b1fe7fdf1747a374fc4000b9df6a28502b3ef7c
xmatute- <xmatute-@student.42urduliz.com>
2024-10-18 12:07:03 +0200

    ahora numbers no pone un salto de linea de mas

--- m01/ex01/number.py
+++ m01/ex01/number.py
@@ -6,13 +6,13 @@
 #    By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/14 20:40:56 by xmatute-          #+#    #+#              #
-#    Updated: 2024/10/14 20:58:56 by xmatute-         ###   ########.fr        #
+#    Updated: 2024/10/18 12:06:19 by xmatute-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 def put_numbers_from_file():
     with open("numbers.txt", "r") as file:
-        print("\n".join(file.read().split(',')))
+        print("\n".join(file.read().split(',')), end='')
 
 if __name__ == "__main__":
    put_numbers_from_file()
