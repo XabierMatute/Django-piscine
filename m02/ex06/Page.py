3e883b88fb55afee3d96edfdd6862f69f0d04ac6
xmatute- <xmatute-@student.42urduliz.com>
2024-10-22 11:47:20 +0200

    mas tests

--- m02/ex06/Page.py
+++ m02/ex06/Page.py
@@ -6,7 +6,7 @@
 #    By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/21 12:33:03 by xmatute-          #+#    #+#              #
-#    Updated: 2024/10/21 13:40:20 by xmatute-         ###   ########.fr        #
+#    Updated: 2024/10/22 11:46:52 by xmatute-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,45 +22,45 @@ class Page:
             if isinstance(elem, Text):
                 return True
             if elem.tag not in ['html', 'head', 'body', 'title', 'meta', 'img', 'table', 'th', 'tr', 'td', 'ul', 'ol', 'li', 'h1', 'h2', 'p', 'div', 'span', 'hr', 'br']:
-                print(f'Invalid tag: {elem.tag}')
+                # print(f'Invalid tag: {elem.tag}')
                 return False
             if elem.tag == 'html':
                 if len(elem.content) != 2 or not isinstance(elem.content[0], Head) or not isinstance(elem.content[1], Body):
-                    print(f'Invalid html content: {elem.content}')
+                    # print(f'Invalid html content: {elem.content}')
                     return False
             if elem.tag == 'head':
                 if len([e for e in elem.content if isinstance(e, Title)]) != 1:
-                    print(f'Invalid head content: {elem.content}')
+                    # print(f'Invalid head content: {elem.content}')
                     return False
             if elem.tag in ['body', 'div']:
                 if not all(isinstance(e, (H1, H2, Div, Table, Ul, Ol, Span, Text)) for e in elem.content):
-                    print(f'Invalid body content: {elem.content}')
+                    # print(f'Invalid body content: {elem.content}')
                     return False
             if elem.tag in ['title', 'h1', 'h2', 'li', 'th', 'td']:
                 if len(elem.content) != 1 or not isinstance(elem.content[0], Text):
-                    print(f'Invalid {elem.tag} content: {elem.content}')
+                    # print(f'Invalid {elem.tag} content: {elem.content}')
                     return False
             if elem.tag == 'p':
                 if not all(isinstance(e, Text) for e in elem.content):
-                    print(f'Invalid p content: {elem.content}')
+                    # print(f'Invalid p content: {elem.content}')
                     return False
             if elem.tag == 'span':
                 if not all(isinstance(e, (Text, P)) for e in elem.content):
-                    print(f'Invalid span content: {elem.content}')
+                    # print(f'Invalid span content: {elem.content}')
                     return False
             if elem.tag in ['ul', 'ol']:
                 if not elem.content or not all(isinstance(e, Li) for e in elem.content):
-                    print(f'Invalid {elem.tag} content: {elem.content}')
+                    # print(f'Invalid {elem.tag} content: {elem.content}')
                     return False
             if elem.tag == 'tr':
                 ths = [e for e in elem.content if isinstance(e, Th)]
                 tds = [e for e in elem.content if isinstance(e, Td)]
                 if not elem.content or (ths and tds) or (ths and tds):
-                    print(f'Invalid tr content: {elem.content}')
+                    # print(f'Invalid tr content: {elem.content}')
                     return False
             if elem.tag == 'table':
                 if not elem.content or not all(isinstance(e, Tr) for e in elem.content):
-                    print(f'Invalid table content: {elem.content}')
+                    # print(f'Invalid table content: {elem.content}')
                     return False
             return all(validate(e) for e in elem.content)
 
@@ -75,20 +75,164 @@ class Page:
         with open(filename, 'w') as file:
             file.write(str(self))
 
-# Tests
-def test_page():
-    title = Title([Text("My Page")])
-    head = Head([title])
-    body = Body([H1([Text("Welcome")]), Span([P([Text("This is a paragraph.")]), P([Text("This is another paragraph.")])])])
-    html = Html([head, body])
-    page = Page(html)
-    
-    assert page.is_valid() == True
-    print(page)
 
-    page.write_to_file("output.html")
+def test():
+    page = Page(Html([
+    Head([Title(Text('"Hello ground!"')), Meta({'charset': 'UTF-8'})]),
+    Body([])
+    ]))
+    if page.is_valid():
+        print('\nvalid page (example)')
+        print(page)
+    
+    page = Page(Html([
+    Head([Title(Text('"Hello ground!"')), Meta({'charset': 'UTF-8'}), Elem('tortilla de patata')]),
+    Body()
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (invalid tag)')
+        print(page)
+        print()
+    
+    page = Page(Html([
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (no head not body)')
+        print(page)
+        print
+
+    page = Page(Html([
+    Body([])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (no head)')
+        print(page)
+        print()
+
+    page = Page(Html([
+    Head([Title(Text('"Hello ground!"')), Meta({'charset': 'UTF-8'})]),
+    Head([Title(Text('"Bye ground!"')), Meta({'charset': 'UTF-8'})]),
+    Body([])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (2 heads)')
+        print(page)
+        print()
+
+    page = Page(Html([
+    Head([]),
+    Body([])
+    ]))
+    if not page.is_valid():
+        print('\ninvalid page (no tittle tags)')
+        print(page)
+        print()
+
+    page = Page(Html([
+        Head([Title(Text('"Hello ground!"')), Title(Text('"Hello ground!"')), Meta({'charset': 'UTF-8'})]),
+        Body([])
+    ]))
+    if not page.is_valid():
+        print('\ninvalid page (2 tittle tags)')
+        print(page)
+        print()
+
+    page = Page(Html([
+    Head([Title(Text('"Hello ground!"'))]),
+    Body([Meta({'charset': 'UTF-8'})])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (meta in body)')
+        print(page)
+        print()
+
+    page = Page(Html([
+    Head([Title([Text('"Hello ground!"'), Text('"Helllo!"')])]),
+    Body([])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (2 texts in title)')
+        print(page)
+        print()
+
+    page = Page(Html([
+    Head([Title(H1(Text('"Hello ground!"')))]),
+    Body([])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (h1 in title)')
+        print(page)
+        print()
+
+    page = Page(Html([
+        Head([Title(Text('"Hello ground!"'))]),
+        Body([P(Text('"Not allowed here!"'))])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (p in body)')
+        print(page)
+        print()
+
+    page = Page(Html([
+        Head([Title(Text('"Hello ground!"'))]),
+        Body([P(H1(Text('"Not allowed here!"')))])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (h1 in p)')
+        print(page)
+        print()
+
+    page = Page(Html([
+        Head([Title(Text('"Hello ground!"'))]),
+        Body([Span(H1(Text('"Not allowed here!"')))])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (h1 in span)')
+        print(page)
+        print()
+
+    page = Page(Html([
+        Head([Title(Text('"Hello ground!"'))]),
+        Body([Ul(H1(Text('"Not allowed here!"')))])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (h1 in ul)')
+        print(page)
+        print()
+
+    page = Page(Html([
+        Head([Title(Text('"Hello ground!"'))]),
+        Body([Table([Tr([Th(Text('"Header"')), Td(Text('"Data"'))])])])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (mix of th and td in tr)')
+        print(page)
+        print()
+
+    page = Page(Html([
+        Head([Title(Text('"Hello ground!"'))]),
+        Body([Table([H1(Text('"Not allowed here!"'))])])
+    ]))
+
+    if not page.is_valid():
+        print('\ninvalid page (h1 in table)')
+        print(page)
+        print()
 
 def main():
+    test()
     page = Page(Html([
         Head(Title(Text('"Hello ground!"'))),
         Body([
