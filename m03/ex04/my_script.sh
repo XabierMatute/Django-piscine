3d62153101f2b48906bb919290c17b44e17493d1
xmatute- <xmatute-@student.42urduliz.com>
2024-11-14 12:01:07 +0100

    ex04

---
+++ m03/ex04/my_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Verificar si virtualenv está instalado, si no, instalarlo en el directorio de usuario
+if ! python3 -m virtualenv --version &> /dev/null; then
+    echo "virtualenv no está instalado. Instalándolo en el directorio de usuario..."
+    pip install --user virtualenv
+    export PATH=$PATH:~/.local/bin
+fi
+
+# Crear el entorno virtual con virtualenv y nombrarlo "django_venv"
+python3 -m virtualenv django_venv
+
+# Verificar si el entorno virtual se creó correctamente
+if [ ! -d "django_venv" ]; then
+    echo "The virtual environment was not created correctly"
+    exit 1
+fi
+
+# Activar el entorno virtual
+source django_venv/bin/activate
+
+# Instalar las dependencias de requirement.txt
+pip install -r requirement.txt
+
+# Mantener el entorno virtual activado al salir del script
+exec "$SHELL"
